15015d1d3cb8f7778230e44e4f19c53bd4d89f75
nir <nvitlem@gmail.com>
2022-05-13 13:38:03 +0300

    camera2 api end setup

--- app/src/main/java/com/example/nvpromter/MainActivity.kt
+++ app/src/main/java/com/example/nvpromter/MainActivity.kt
@@ -8,8 +8,6 @@ import android.content.pm.PackageManager
 import android.graphics.Color
 import android.graphics.ImageFormat
 import android.graphics.SurfaceTexture
-import android.hardware.Camera
-import android.hardware.Camera.CameraInfo
 import android.hardware.camera2.*
 import android.hardware.camera2.params.StreamConfigurationMap
 import android.icu.text.SimpleDateFormat
@@ -17,27 +15,18 @@ import android.media.CamcorderProfile
 import android.media.Image
 import android.media.ImageReader
 import android.media.MediaRecorder
-import android.net.Uri
 import android.os.*
-import android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE
-import android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO
-import android.provider.Settings
 import android.util.Log
 import android.util.Size
 import android.util.SparseIntArray
 import android.view.Surface
 import android.view.TextureView
 import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.io.File
-import java.io.IOException
-import java.lang.reflect.Field
 import java.util.*
 
 const val CAMERA_REQUEST_RESULT = 101
@@ -46,6 +35,27 @@ private val PERMISSION_REQUEST_CODE = 101;
 private val TAG = "Permission";
 private var bTnStatus :Boolean = true;
 private var runningBackgroundThread : Boolean =false;
+private var  surfaceTexture : SurfaceTexture? = null
+private var previewSurface: Surface?=null
+private var cameraCharacteristics : CameraCharacteristics?=null
+private  var textureView: TextureView? = null
+private var backgroundHandlerThread: HandlerThread?= null
+private var backgroundHandler: Handler? = null
+private  var cameraManager: CameraManager? = null
+private  var cameraDevice: CameraDevice? = null
+private  var captureRequestBuilder: CaptureRequest.Builder? = null
+private  var cameraCaptureSession: CameraCaptureSession? = null
+private  var imageReader: ImageReader? = null
+private  var previewSize: Size? = null
+private  var videoSize: Size? = null
+private var shouldProceedWithOnResume: Boolean = true
+private var CameraFront : Boolean = false
+private var orientations : SparseIntArray = SparseIntArray(4).apply {
+    append(Surface.ROTATION_0, 0)
+    append(Surface.ROTATION_90, 90)
+    append(Surface.ROTATION_180, 180)
+    append(Surface.ROTATION_270, 270)
+}
 
 var cameraCount : Int = 0;
 var cameraIndex :  Int = 0;
@@ -58,23 +68,7 @@ class MainActivity : AppCompatActivity() {
         var ScrollTextViewObject: ScrollTextView? = null
     }
 
-    private lateinit var textureView: TextureView
-    private lateinit var backgroundHandlerThread: HandlerThread
-    private lateinit var backgroundHandler: Handler
-    private lateinit var cameraManager: CameraManager
-    private lateinit var cameraDevice: CameraDevice
-    private lateinit var captureRequestBuilder: CaptureRequest.Builder
-    private lateinit var cameraCaptureSession: CameraCaptureSession
-    private lateinit var imageReader: ImageReader
-    private lateinit var previewSize: Size
-    private lateinit var videoSize: Size
-    private var shouldProceedWithOnResume: Boolean = true
-    private var orientations : SparseIntArray = SparseIntArray(4).apply {
-        append(Surface.ROTATION_0, 0)
-        append(Surface.ROTATION_90, 90)
-        append(Surface.ROTATION_180, 180)
-        append(Surface.ROTATION_270, 270)
-    }
+
 
     private lateinit var mediaRecorder: MediaRecorder
     private var isRecording: Boolean = false
@@ -88,27 +82,18 @@ class MainActivity : AppCompatActivity() {
 
             textureView = findViewById<TextureView>(R.id.tVcamera_preview)
             cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
-            cameraCount= cameraManager.cameraIdList.size
+            cameraCount= cameraManager!!.cameraIdList.size
 
             val btn_camera = findViewById<Button>(R.id.CameraB)
             // set on-click listener
             btn_camera.setOnClickListener {
 
-                if (cameraIndex == (cameraCount + 1)) cameraIndex = 0;
-                if (runningBackgroundThread) {
-                   // stopBackgroundThread()
-                    setupCamera()
-                    connectCamera()
-                  //  startBackgroundThread()
-                } else {
-                    setupCamera()
-                    connectCamera()
-                   // startBackgroundThread()
-                }
+                if (cameraIndex == (cameraCount )) cameraIndex = 0;
+                setupCamera()
+                connectCamera()
                 cameraIndex += 1;
 
             }
-
             // get reference to button
             val btn_click = findViewById<Button>(R.id.button_capture)
             // set on-click listener
@@ -161,79 +146,112 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         startBackgroundThread()
-        if (textureView.isAvailable && shouldProceedWithOnResume) {
+        if (textureView!!.isAvailable && shouldProceedWithOnResume) {
             setupCamera()
-        } else if (!textureView.isAvailable){
-            textureView.surfaceTextureListener = surfaceTextureListener
+        } else if (!textureView!!.isAvailable){
+            textureView!!.surfaceTextureListener = surfaceTextureListener
         }
         shouldProceedWithOnResume = !shouldProceedWithOnResume
     }
 
     private fun setupCamera() {
-        Log.d("setupCamera-cameraIndex",cameraIndex.toString())
-       // val cameraIds: Array<String> = cameraManager.cameraIdList
+        Log.d("setupCamera-cameraIndex", cameraIndex.toString())
+        // val cameraIds: Array<String> = cameraManager.cameraIdList
 
-       // for (id in cameraIds) {
-            val cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraIndex.toString())
+        // for (id in cameraIds) {
+        if ( surfaceTexture!=null)
+        {
+         //   if (!surfaceTexture!!.isReleased ) surfaceTexture!!.release()
+        }
 
-            //If we want to choose the rear facing camera instead of the front facing one
-          //  if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT) {
-         //       continue
-         //   }
+        if ( cameraDevice!= null)
+        {
+            cameraDevice!!.close()
+            cameraDevice = null;
+        }
+        cameraCharacteristics = cameraManager?.getCameraCharacteristics(cameraIndex.toString())
 
-            val streamConfigurationMap : StreamConfigurationMap? = cameraCharacteristics.get(
-                CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+        //If we want to choose the rear facing camera instead of the front facing one
+        CameraFront = cameraCharacteristics?.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT
 
-            if (streamConfigurationMap != null) {
-                previewSize = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!.getOutputSizes(
-                    ImageFormat.JPEG).maxByOrNull { it.height * it.width }!!
-                videoSize = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!.getOutputSizes(MediaRecorder::class.java).maxByOrNull { it.height * it.width }!!
-                imageReader = ImageReader.newInstance(previewSize.width, previewSize.height, ImageFormat.JPEG, 1)
-                imageReader.setOnImageAvailableListener(onImageAvailableListener, backgroundHandler)
-        //    }
+        val streamConfigurationMap: StreamConfigurationMap? = cameraCharacteristics!!.get(
+            CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP
+        )
+
+        if (streamConfigurationMap != null) {
+            previewSize =
+                cameraCharacteristics!!.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!
+                    .getOutputSizes(
+                        ImageFormat.JPEG
+                    ).maxByOrNull { it.height * it.width }!!
+            videoSize =
+                cameraCharacteristics!!.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!
+                    .getOutputSizes(MediaRecorder::class.java)
+                    .maxByOrNull { it.height * it.width }!!
+            imageReader = ImageReader.newInstance(
+                previewSize!!.width,
+                previewSize!!.height,
+                ImageFormat.JPEG,
+                1
+            )
+            imageReader!!.setOnImageAvailableListener(onImageAvailableListener, backgroundHandler)
+            //    }
         }
     }
 
     private fun takePhoto() {
-        captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
-        captureRequestBuilder.addTarget(imageReader.surface)
+        captureRequestBuilder = cameraDevice?.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
+        captureRequestBuilder?.addTarget(imageReader!!.surface)
         val rotation = windowManager.defaultDisplay.rotation
-        captureRequestBuilder.set(CaptureRequest.JPEG_ORIENTATION, orientations.get(rotation))
-        cameraCaptureSession.capture(captureRequestBuilder.build(), captureCallback, null)
+        captureRequestBuilder!!.set(CaptureRequest.JPEG_ORIENTATION, orientations.get(rotation))
+        cameraCaptureSession!!.capture(captureRequestBuilder!!.build(), captureCallback, null)
     }
 
     @SuppressLint("MissingPermission")
     private fun connectCamera() {
-        cameraManager.openCamera(cameraIndex.toString(), cameraStateCallback, backgroundHandler)
+        cameraManager!!.openCamera(cameraIndex.toString(), cameraStateCallback, backgroundHandler)
     }
 
     private fun setupMediaRecorder() {
         mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE)
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
         mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
         mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264)
-        mediaRecorder.setVideoSize(videoSize.width, videoSize.height)
+        mediaRecorder.setVideoSize(videoSize!!.width, videoSize!!.height)
         mediaRecorder.setVideoFrameRate(30)
+        if (!CameraFront) {
+            // Back
+            mediaRecorder.setOrientationHint(90);
+        } else {
+            // Front
+            mediaRecorder.setOrientationHint(270);
+        }
         mediaRecorder.setOutputFile(createFile()?.absolutePath)
         mediaRecorder.setVideoEncodingBitRate(10_000_000)
         mediaRecorder.prepare()
     }
 
     private fun startRecording() {
-        val surfaceTexture : SurfaceTexture? = textureView.surfaceTexture
-        surfaceTexture?.setDefaultBufferSize(previewSize.width, previewSize.height)
+        val surfaceTexture: SurfaceTexture? = textureView!!.surfaceTexture
+        surfaceTexture?.setDefaultBufferSize(previewSize!!.width, previewSize!!.height)
         val previewSurface: Surface = Surface(surfaceTexture)
         val recordingSurface = mediaRecorder.surface
 
-        /*  PMText = findViewById<ScrollTextView>(R.id.PtextView)
-               PMText!!.setTextToShow(ScrollTextViewObject?.text.toString())
-               PMText!!.setTextColor(Color.RED);
-               PMText?.startScroll();*/
+       /* PMText = findViewById<ScrollTextView>(R.id.PtextView)
+        PMText!!.setTextToShow(ScrollTextViewObject?.text.toString())
+        PMText!!.setTextColor(Color.RED);
+        PMText?.startScroll();*/
 
-        captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_RECORD)
-        captureRequestBuilder.addTarget(previewSurface)
-        captureRequestBuilder.addTarget(recordingSurface)
+        captureRequestBuilder = cameraDevice?.createCaptureRequest(CameraDevice.TEMPLATE_RECORD)
+        captureRequestBuilder!!.addTarget(previewSurface)
+        captureRequestBuilder!!.addTarget(recordingSurface)
 
-        cameraDevice.createCaptureSession(listOf(previewSurface, recordingSurface), captureStateVideoCallback, backgroundHandler)
+        cameraDevice!!.createCaptureSession(
+            listOf(previewSurface, recordingSurface),
+            captureStateVideoCallback,
+            backgroundHandler
+        )
     }
 
     /**
@@ -246,6 +264,8 @@ class MainActivity : AppCompatActivity() {
             if (wasCameraPermissionWasGiven()) {
                 setupCamera()
                 connectCamera()
+                if (cameraIndex == (cameraCount + 1)) cameraIndex = 0;
+                cameraIndex++
             }
         }
 
@@ -268,19 +288,24 @@ class MainActivity : AppCompatActivity() {
 
     private val cameraStateCallback = object : CameraDevice.StateCallback() {
         override fun onOpened(camera: CameraDevice) {
+            Log.d("cameraStateCallback-onOpened",camera.id.toString() )
             cameraDevice = camera
-            val surfaceTexture : SurfaceTexture? = textureView.surfaceTexture
-            surfaceTexture?.setDefaultBufferSize(previewSize.width, previewSize.height)
-            val previewSurface: Surface = Surface(surfaceTexture)
+            surfaceTexture = textureView!!.surfaceTexture
+            surfaceTexture?.setDefaultBufferSize(previewSize!!.width, previewSize!!.height)
+            previewSurface = Surface(surfaceTexture)
+            captureRequestBuilder = cameraDevice?.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+            captureRequestBuilder!!.addTarget(previewSurface!!)
 
-            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-            captureRequestBuilder.addTarget(previewSurface)
-
-            cameraDevice.createCaptureSession(listOf(previewSurface, imageReader.surface), captureStateCallback, null)
+            cameraDevice!!.createCaptureSession(listOf(previewSurface, imageReader!!.surface), captureStateCallback, null)
         }
 
         override fun onDisconnected(cameraDevice: CameraDevice) {
+            Log.d("cameraStateCallback-onDisconnected",cameraDevice.id.toString() )
+        }
 
+        override fun onClosed(camera: CameraDevice) {
+            super.onClosed(camera)
+            Log.d("cameraStateCallback-onClosed",camera.id.toString() )
         }
 
         override fun onError(cameraDevice: CameraDevice, error: Int) {
@@ -293,6 +318,7 @@ class MainActivity : AppCompatActivity() {
                 else -> "Unknown"
             }
             Log.e(TAG, "Error when trying to connect camera $errorMsg")
+            Log.d("cameraStateCallback-onError", "Error when trying to connect camera $errorMsg " + cameraDevice.id.toString())
         }
     }
 
@@ -303,15 +329,15 @@ class MainActivity : AppCompatActivity() {
         Log.d("startBackgroundThread-cameraIndex",cameraIndex.toString())
         runningBackgroundThread = true;
         backgroundHandlerThread = HandlerThread("CameraVideoThread")
-        backgroundHandlerThread.start()
-        backgroundHandler = Handler(backgroundHandlerThread.looper)
+        backgroundHandlerThread!!.start()
+        backgroundHandler = Handler(backgroundHandlerThread!!.looper)
     }
 
     private fun stopBackgroundThread() {
         Log.d("stopBackgroundThread-cameraIndex",cameraIndex.toString())
         runningBackgroundThread = false
-        backgroundHandlerThread.quitSafely()
-        backgroundHandlerThread.join()
+        backgroundHandlerThread!!.quitSafely()
+        backgroundHandlerThread!!.join()
     }
 
     /**
@@ -320,13 +346,14 @@ class MainActivity : AppCompatActivity() {
 
     private val captureStateCallback = object : CameraCaptureSession.StateCallback() {
         override fun onConfigureFailed(session: CameraCaptureSession) {
-
+            Log.d("captureStateCallback-onConfigureFailed", session.device.id.toString())
         }
         override fun onConfigured(session: CameraCaptureSession) {
+            Log.d("captureStateCallback-onConfigured", session.device.id.toString())
             cameraCaptureSession = session
 
-            cameraCaptureSession.setRepeatingRequest(
-                captureRequestBuilder.build(),
+            cameraCaptureSession!!.setRepeatingRequest(
+                captureRequestBuilder!!.build(),
                 null,
                 backgroundHandler
             )
@@ -335,14 +362,16 @@ class MainActivity : AppCompatActivity() {
 
     private val captureStateVideoCallback = object : CameraCaptureSession.StateCallback() {
         override fun onConfigureFailed(session: CameraCaptureSession) {
+            Log.d("captureStateVideoCallback-onConfigureFailed", session.device.id.toString())
             Log.e(TAG, "Configuration failed")
         }
         override fun onConfigured(session: CameraCaptureSession) {
+            Log.d("captureStateVideoCallback-onConfigured", session.device.id.toString())
             cameraCaptureSession = session
-            captureRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureResult.CONTROL_AF_MODE_CONTINUOUS_VIDEO)
+            captureRequestBuilder?.set(CaptureRequest.CONTROL_AF_MODE, CaptureResult.CONTROL_AF_MODE_CONTINUOUS_VIDEO)
             try {
-                cameraCaptureSession.setRepeatingRequest(
-                    captureRequestBuilder.build(), null,
+                cameraCaptureSession!!.setRepeatingRequest(
+                    captureRequestBuilder!!.build(), null,
                     backgroundHandler
                 )
                 mediaRecorder.start()
@@ -366,20 +395,24 @@ class MainActivity : AppCompatActivity() {
             request: CaptureRequest,
             timestamp: Long,
             frameNumber: Long
-        ) {}
+        ) {
+            Log.d("captureCallback-onCaptureStarted", session.device.id.toString())
+        }
 
         override fun onCaptureProgressed(
             session: CameraCaptureSession,
             request: CaptureRequest,
             partialResult: CaptureResult
-        ) { }
+        ) {
+            Log.d("captureCallback-onCaptureProgressed", session.device.id.toString())
+        }
 
         override fun onCaptureCompleted(
             session: CameraCaptureSession,
             request: CaptureRequest,
             result: TotalCaptureResult
         ) {
-
+            Log.d("captureCallback-onCaptureCompleted", session.device.id.toString())
         }
     }
 
